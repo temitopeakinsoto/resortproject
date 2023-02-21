fb25748ec33861a2b01b036b6ee8bec90b5a1c77
temitopeakinsoto <takinsoto@gmail.com>
2023-02-21 20:55:29 +0000

    resort project initial push for completed class files

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import bluej.ResortUI;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //System.out.println("Hello world!");
+        ResortUI xx = new ResortUI();
+        xx.runUI();
+
+    }
+
+}
