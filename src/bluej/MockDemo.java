7bd05e1f9ba5d60987c0c3dd02dd1f1eb952f350
temitopeakinsoto <takinsoto@gmail.com>
2023-03-06 23:53:13 +0000

    Add mock demo file

---
+++ src/bluej/MockDemo.java
@@ -0,0 +1,16 @@
+package bluej;
+
+/**
+ * Write a description of class MockDemo here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MockDemo
+{
+    public static void main(String[] args)
+    {
+        Resort haven = new Resort("Fortunate Islands");
+        
+    }
+}
